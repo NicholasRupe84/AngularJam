74b3f6c65725d1ead1401280e7285ba019c04ab5
Mohammad Doleh <mdoleh@kent.edu>
2015-09-10 14:54:51 -0400

    Angular basic setup

---
+++ DestinationMarketplace/DestinationMarketplace/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DestinationMarketplace.Startup))]
+namespace DestinationMarketplace
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ DestinationMarketplaceSPA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="App.DestinationMarketplaceSPA.Application" Language="C#" %>
